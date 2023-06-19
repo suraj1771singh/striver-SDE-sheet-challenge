d7d8ff806768b0f1e479bb183d1c328543aaf96c
Suraj Singh <absurajsinghchamiyal@gmail.com>
2023-06-19 23:56:42 +0530

    day 10

---
+++ 67 Search In Rotated Sorted Array/Solution1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int search(int a[], int target) {
+        int l = 0, h = a.length - 1;
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (a[mid] == target)
+                return mid;
+            if (a[l] <= a[mid]) {
+                if (target >= a[l] && target < a[mid])
+                    h = mid - 1;
+                else
+                    l = mid + 1;
+            } else {
+                if (target > a[mid] && target <= a[h])
+                    l = mid + 1;
+                else
+                    h = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
