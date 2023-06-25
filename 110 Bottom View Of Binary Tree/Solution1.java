67f0684bb834c9fe0b15521bef4c1d85f44ade7c
Suraj Singh <absurajsinghchamiyal@gmail.com>
2023-06-25 23:53:51 +0530

    day 16

---
+++ 110 Bottom View Of Binary Tree/Solution1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Solution {
+    public static ArrayList<Integer> bottomView(BinaryTreeNode root) {
+        // Write your code here.
+        Queue<Pair> q = new ArrayDeque<>();
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        q.add(new Pair(0, root));
+
+        while (!q.isEmpty()) {
+            Pair cur = q.poll();
+
+            map.put(cur.hd, cur.node.val);
+
+            if (cur.node.left != null) {
+                q.add(new Pair(cur.hd - 1, cur.node.left));
+            }
+            if (cur.node.right != null) {
+                q.add(new Pair(cur.hd + 1, cur.node.right));
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            ans.add(entry.getValue());
+        }
+        return ans;
+    }
+
+    static class Pair {
+        int hd;
+        BinaryTreeNode node;
+
+        public Pair(int hd, BinaryTreeNode node) {
+            this.hd = hd;
+            this.node = node;
+        }
+    }
+}
